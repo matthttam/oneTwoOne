19c9a3b6adb8914fc818f54609a92917a202134d
Matt henry <matt.henry@owensboro.kyschools.us>
2023-04-11 10:56:21 -0500

    Corrected spelling. Added owensboro specific rule that when assigned to a teacher, anyone can use it.

--- background.js
+++ background.js
@@ -12,9 +12,9 @@ const asPromised = (block) => {
 
 entGetInfo = function (item) {
   return asPromised((callback) => {
-    if ( typeof chrome.enterprise !== "undefined" ) {
-      if ( typeof chrome.enterprise.deviceAttributes !== "undefined" ) {
-        if ( typeof chrome.enterprise.deviceAttributes[item] !== "undefined" ) {
+    if (typeof chrome.enterprise !== "undefined") {
+      if (typeof chrome.enterprise.deviceAttributes !== "undefined") {
+        if (typeof chrome.enterprise.deviceAttributes[item] !== "undefined") {
           chrome.enterprise.deviceAttributes[item](callback);
         } else { callback(undefined); }
       } else { callback(undefined); }
@@ -31,11 +31,11 @@ getIden = function () {
 function get_data(callback) {
   data = {};
   data_needed = ['location', 'assetid', 'directoryid', 'useremail'];
-  mypromises = [ Promise.resolve(false),  // 0 location
-                 Promise.resolve(false),  // 1 asset id
-                 Promise.resolve(false),  // 2 directory api id
-                 Promise.resolve(false),  // 3 user email
-                ];
+  mypromises = [Promise.resolve(false),  // 0 location
+  Promise.resolve(false),  // 1 asset id
+  Promise.resolve(false),  // 2 directory api id
+  Promise.resolve(false),  // 3 user email
+  ];
   for (i = 0; i < data_needed.length; i++) {
     if (data_needed[i] === 'location') {
       mypromises[0] = entGetInfo('getDeviceAnnotatedLocation');
@@ -47,7 +47,7 @@ function get_data(callback) {
       mypromises[3] = getIden();
     }
   }
-  Promise.all(mypromises).then(function(values) {
+  Promise.all(mypromises).then(function (values) {
     if (values[0]) {
       data.location = values[0].toLowerCase().split(',');
     }
@@ -67,37 +67,37 @@ function get_data(callback) {
 var block_everything;
 
 function decide_if_blocking(data) {
-    console.log(data);
-    if (typeof data.location == 'undefined') {
-      // unmanaged device
-      console.log('couldn\'t get managed device info. Is this device enrolled in your admin console and device location set? Not blocking anything')
-      block_everything = false;
-      return;
-    }
-    if (data.location.includes('*')) {
-      console.log('Device allows wildcard login, not blocking anything.');
-      block_everything = false;
-    } else if (data.location.includes(data.useremail)) {
-      console.log('Device has this user as allowed to login, not blocking anything.');
-      block_everything = false;
-    } else {
-      console.log('Device does not have this user as allowed, BLOCKING ALL WEBSITES!');
-      alert('You are not allowed to use this device. Please log out.');
-      block_everything = true;
-    }
+  console.log(data);
+  if (typeof data.location == 'undefined') {
+    // unmanaged device
+    console.log('couldn\'t get managed device info. Is this device enrolled in your admin console and device location set? Not blocking anything')
+    block_everything = false;
+    return;
   }
+  if (data.location.includes('*') || data.location.includes('@owensboro.kyschools.us')) {
+    console.log('Device allows wildcard login, not blocking anything.');
+    block_everything = false;
+  } else if (data.location.includes(data.useremail)) {
+    console.log('Device has this user as allowed to login, not blocking anything.');
+    block_everything = false;
+  } else {
+    console.log('Device does not have this user as allowed, BLOCKING ALL WEBSITES!');
+    alert('You are not allowed to use this device. Please log out.');
+    block_everything = true;
+  }
+}
 
-chrome.runtime.onStartup.addListener(function() {
-  console.log('determing blocking status on startup.')
+chrome.runtime.onStartup.addListener(function () {
+  console.log('determining blocking status on startup.')
   get_data(decide_if_blocking);
 })
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   console.log('determine blocking status on install.')
   get_data(decide_if_blocking);
 })
 
-function check_block({frameId, url}) {
+function check_block({ frameId, url }) {
   if (block_everything) {
     url = chrome.extension.getURL("blocked.html")
     return { redirectUrl: url };
@@ -107,6 +107,6 @@ function check_block({frameId, url}) {
 }
 
 chrome.webRequest.onBeforeRequest.addListener(check_block, {
-	urls: ["*://*/*"],
-	types: ["main_frame", "sub_frame"]
+  urls: ["*://*/*"],
+  types: ["main_frame", "sub_frame"]
 }, ["blocking"]);
